ccaa83d0b330b4e4bcacde8c058a1c3b0d870bc2
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-05-28 16:02:45 +0200

    Add files via upload

---
+++ global.js
@@ -0,0 +1,32 @@
+window.repo = "PCG06/pokeyaeeh/YAEEH"
+window.checkUpdate = "11 YAEEH"
+
+
+fetch('https://raw.githubusercontent.com/ydarissep/dex-core/main/index.html').then(async response => {
+	return response.text()
+}).then(async rawHTMLText => {
+	const parser = new DOMParser()
+	const doc = parser.parseFromString(rawHTMLText, 'text/html')
+    document.querySelector('html').innerHTML = doc.querySelector('html').innerHTML
+
+
+    document.title = "YAEHH Dex"
+    document.getElementById("footerName").innerText = "YAEEH idk something\nYdarissep Pokedex"
+
+    document.getElementById("speciesPanelTMHMTable").innerHTML = document.getElementById("speciesPanelTMHMTable").innerHTML.replace("TM/HM", "Teachable")
+    document.getElementById("speciesPanelTutorTable").classList.add("hide")
+
+
+    await fetch("https://raw.githubusercontent.com/ydarissep/dex-core/main/src/global.js").then(async response => {
+        return response.text()
+    }).then(async text => {
+        await eval.call(window,text)
+    }).catch(error => {
+        console.warn(error)
+    })    
+
+}).catch(error => {
+	console.warn(error)
+})
+
+
